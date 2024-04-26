0c46db6ca0de0dc6f6bb2fef6dfbd7f6b70e5232
wellheythereMJ <abetterplace@outlook.com>
2024-04-26 16:32:56 -0700

    updated schema, and server.js still need a few more functions to complete

--- db/schema.sql
+++ db/schema.sql
@@ -8,3 +8,20 @@ CREATE TABLE department (
     department_name VARCHAR(100) NOT NULL
   );
 
+CREATE TABLE role (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR(300) NOT NULL,
+  salary DECIMAL(10,2) NOT NULL,
+  department_id INT NOT NULL,
+  FOREIGN KEY (department_id) REFERENCES department(id)
+);
+
+CREATE TABLE employee (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(300) NOT NULL,
+  last_name VARCHAR(300) NOT NULL,
+  role_id INT,
+  manager_id INT,
+  FOREIGN KEY (role_id) REFERENCES role(id),
+  FOREIGN KEY (manager_id) REFERENCES employee(id)
+  );
--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const addToDepartment = [{
   message: "What is the name of the new department?"
 }]
 
-const addEmployee= [
+const addEmployeeQuestions= [
   {
   type: "input",
   name: "first_name",
@@ -20,7 +20,7 @@ const addEmployee= [
   }
 ]
 
-const addRole = [
+const addRoleQuestions = (arr) => [
   {
     type: "input",
     name: "title",
@@ -31,6 +31,12 @@ const addRole = [
     name: "salary",
     message: "What is the salary of the new role?"
   },
+  {
+    type: "list",
+    name: "department_id",
+    message: "Which department does this role belong to?",
+    choices: arr
+  }
 ]
 
 const mainMenu = [
@@ -40,12 +46,12 @@ const mainMenu = [
     message: "What would you like to do?",
     choices: [
       "Add a department",
-      // "Add an employee",
-      // "Add a role",
+      "Add an employee",
+      "Add a role",
       "View all departments",
-      // "View all roles",
-      // "View all employees",
-      // "Update an employee role",
+      "View all roles",
+      "View all employees",
+      "Update an employee role",
       "Exit"
     ]
   }
@@ -68,6 +74,38 @@ const addDepartment = () =>{
           })
       })
 }
+
+const viewAllRoles = () => {
+  mysql.promise().query("SELECT * FROM role;")
+  .then(res=>{console.table(res[0])
+  menuList();
+  })
+}
+
+const viewAllEmployees = () => {
+  mysql.promise().query("SELECT * FROM employee;")
+  .then(res=>{console.table(res[0])
+  menuList();
+  })
+}
+
+const addRole = async() => {
+  const departments = await mysql.promise().query("SELECT * FROM department;");
+  const deptArr = departments[0].map((department) => ({
+    name: department.department_name,
+    value: department.id,
+  }) )
+  inquirer
+     .prompt(addRoleQuestions(deptArr))
+     .then(({title, salary, department_id})=>{
+        mysql.promise().query(`INSERT INTO role(title, salary, department_id) VALUE ('${title}', ${salary}, ${department_id});`)
+        .then(res=>{
+            console.log(`Added ${title} to database`);
+            menuList();
+        })
+    })
+}
+
 const options = (response) => {
   switch (response.main_menu) {
     case 'View all departments':
@@ -76,6 +114,21 @@ const options = (response) => {
     case 'Add a department':
     addDepartment();
     break;
+    case 'View all roles':
+      viewAllRoles();
+      break;
+    case 'Add a role':
+      addRole();
+      break;
+    case 'View all employees':
+      viewAllEmployees();
+      break;
+    case 'Add an employee':
+      addEmployee();
+      break;
+    case 'Update an employee role':
+      updateEmployeeRole();
+      break;
     case 'Exit':
     mysql.end();
     break;
